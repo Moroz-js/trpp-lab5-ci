15d0cafa6a28dee6979e93dcc76a52ea234689dd
Vlad <hotsru12@gmail.com>
2026-06-18 18:17:28 +0300

    test: intentional failure for CI demo

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,7 +1,7 @@
 const { add, subtract, multiply, divide } = require('../src/calculator');
 
 describe('calculator', () => {
-  test('add', () => expect(add(2, 3)).toBe(5));
+  test('add', () => expect(add(2, 3)).toBe(999));
   test('subtract', () => expect(subtract(5, 2)).toBe(3));
   test('multiply', () => expect(multiply(4, 3)).toBe(12));
   test('divide', () => expect(divide(10, 2)).toBe(5));
